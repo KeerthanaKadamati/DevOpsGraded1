86f8b11ddb216316b6467d78eeee3b5ae621b453
KeerthanaKadamati <k.keerthana@hcl.com>
2021-11-22 14:22:10 +0530

    uploaded

---
+++ springboot-jenkins-1/src/main/java/com/hcl/spring/jenkins/SpringbootJenkins1Application.java
@@ -0,0 +1,14 @@
+package com.hcl.spring.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkins1Application.class, args);
+		System.out.println("Hello Devops....");
+	}
+
+}
---
+++ springboot-jenkins-1/src/test/java/com/hcl/spring/jenkins/SpringbootJenkins1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.spring.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkins1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
